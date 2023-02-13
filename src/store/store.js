54888ff3e14bae89f08918d0f5221b487b1f7fa1
Lilibeth Sequeira <Lilibeth.Sequeira@ooqia.com>
2023-02-13 13:54:14 -0600

    refactor: Normalization of entities and data to be used in the app

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,49 @@
-import { reactive } from 'vue'
-
-const store = reactive({
-  username: 'licary',
-  updateUsername(username) {
-    this.username = username
-  }
-})
-
-export default store
+const store = {
+  profile: {
+    username: ''
+  },
+  contacts: [
+    {
+      id: 1,
+      username: 'User 1'
+    },
+    {
+      id: 2,
+      username: 'User 2'
+    }
+  ],
+  messages: [
+    {
+      id: 1,
+      author: 2,
+      message: 'Hola :D'
+    },
+    {
+      id: 2,
+      author: 1,
+      message: 'Hey!!'
+    },
+    {
+      id: 3,
+      author: 2,
+      message: 'Hola :D'
+    },
+    {
+      id: 4,
+      author: 1,
+      message: 'Hey!!'
+    }
+  ],
+  channels: [
+    {
+      id: 1,
+      title: 'Canal 1',
+      messages: [1, 2]
+    },
+    {
+      id: 2,
+      title: 'Canal 2',
+      messages: [3, 4]
+    }
+  ]
+}
